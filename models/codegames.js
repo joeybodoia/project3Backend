18daac7e98282db78e3ee80ebb40d21e3000c2f1
Joey Bodoia <jbodoia21@cmc.edu>
2020-10-28 17:25:08 -0700

    added img to codegames schema

--- models/codegames.js
+++ models/codegames.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const codegamesSchema = mongoose.Schema({
     name: {type: String, required: true},
     description: String,
-    url: String
+    url: String,
+    img: String
 })
 
 module.exports = mongoose.model('Codegame', codegamesSchema)
